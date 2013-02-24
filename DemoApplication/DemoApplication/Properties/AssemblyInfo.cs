3cc9a350172344019989d8d7b6cb0ffc16d10fbc
Rod Johnson <rod@scaffr.com>
2013-02-24 15:55:58 -0700

    .gitignore is now working

--- DemoApplication/DemoApplication/Properties/AssemblyInfo.cs
+++ DemoApplication/DemoApplication/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4f4e55bc-8e42-461a-aa52-8cb6ebe72e0b")]
+[assembly: Guid("4f4e55bc-8e42-461a-aa52-8cb6ebe72e1b")]
 
 // Version information for an assembly consists of the following four values:
 //
