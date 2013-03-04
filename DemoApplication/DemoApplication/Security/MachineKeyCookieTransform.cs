54521440cf26e23de30df9cb417e8dea5e3da1f6
Rod Johnson <rod@scaffr.com>
2013-03-04 02:53:17 -0700

    fixes for azure cookie encryption

---
+++ DemoApplication/DemoApplication/Security/MachineKeyCookieTransform.cs
@@ -0,0 +1,20 @@
+﻿namespace DemoApplication.Security
+{
+    using System.IdentityModel;
+    using System.Web.Security;
+
+    public class MachineKeyCookieTransform : CookieTransform
+    {
+        public override byte[] Decode(byte[] encoded)
+        {
+            return MachineKey.Unprotect(encoded);
+            //return MachineKey.Decode(Encoding.UTF8.GetString(encoded), MachineKeyProtection.All);
+        }
+
+        public override byte[] Encode(byte[] value)
+        {
+            return MachineKey.Protect(value);
+            //return Encoding.UTF8.GetBytes(MachineKey.Encode(value, MachineKeyProtection.All));
+        }
+    }
+}
---
+++ DemoApplication/DemoApplication/Security/MachineKeySessionSecurityTokenHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace DemoApplication.Security
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.IdentityModel;
+    using System.IdentityModel.Tokens;
+
+    public class MachineKeySessionSecurityTokenHandler : SessionSecurityTokenHandler
+    {
+        public MachineKeySessionSecurityTokenHandler()
+            : base(CreateTransforms())
+        { }
+
+        public MachineKeySessionSecurityTokenHandler(TimeSpan tokenLifetime)
+            : base(CreateTransforms(), tokenLifetime)
+        { }
+
+        private static ReadOnlyCollection<CookieTransform> CreateTransforms()
+        {
+            return new List<CookieTransform>
+                {
+                    new DeflateCookieTransform(),
+                    new MachineKeyCookieTransform()
+                }.AsReadOnly();
+        }
+    }
+}
