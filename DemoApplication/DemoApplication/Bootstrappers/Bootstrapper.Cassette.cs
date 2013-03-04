49d12b34b8e266626b967f2aa1c0ff01e309f772
radrad <radoslav@everestkc.net>
2013-03-04 01:25:41 -0600

    Fixed Cassette configuration and Nuget restore

--- DemoApplication/DemoApplication/Bootstrappers/Bootstrapper.Cassette.cs
+++ DemoApplication/DemoApplication/Bootstrappers/Bootstrapper.Cassette.cs
@@ -1,23 +1,24 @@
 namespace DemoApplication.Bootstrappers
 {
-    using System.IO;
-    using Cassette;
-    using Cassette.Scripts;
-    using Cassette.Stylesheets;
+	using System.IO;
+	using Cassette;
+	using Cassette.Scripts;
+	using Cassette.Stylesheets;
 
-    public partial class Bootstrapper : IConfiguration<BundleCollection>
+	public partial class Bootstrapper : IConfiguration<BundleCollection>
 	{
-        public void Configure(BundleCollection configurable)
-        {
-            configurable.AddPerSubDirectory<ScriptBundle>("Scripts", new FileSearch()
-            {
-                SearchOption = SearchOption.TopDirectoryOnly
-            });
+		public void Configure(BundleCollection configurable)
+		{
+			configurable.AddPerSubDirectory<ScriptBundle>("Scripts", new FileSearch()
+			{
+				SearchOption = SearchOption.TopDirectoryOnly
+			});
 
-            configurable.AddPerSubDirectory<StylesheetBundle>("content/less");
-            configurable.AddPerSubDirectory<ScriptBundle>("Scripts/custom");
+			//configurable.AddPerSubDirectory<StylesheetBundle>("content/less");
+			configurable.Add<StylesheetBundle>("content/less/site.less");
+			configurable.AddPerSubDirectory<ScriptBundle>("Scripts/custom");
 
-            configurable.AddPerSubDirectory<ScriptBundle>("Scripts/lib");
-        }
+			configurable.AddPerSubDirectory<ScriptBundle>("Scripts/lib");
+		}
 	}
 }
