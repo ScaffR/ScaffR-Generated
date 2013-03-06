268bd488f789fa246804693159764137b95f8852
Rod Johnson <rod@scaffr.com>
2013-03-06 14:09:37 -0700

    cleanup the merged files

--- DemoApplication/DemoApplication/Controllers/Components/ComponentsController.GoogleMaps.cs
+++ DemoApplication/DemoApplication/Controllers/Components/ComponentsController.GoogleMaps.cs
@@ -2,6 +2,8 @@
 
 namespace DemoApplication.Controllers.Components
 {
+    using Core.Common.Geography;
+    using Models.Common;
     using Models.Components;
 
     public partial class ComponentsController
@@ -9,12 +11,22 @@ namespace DemoApplication.Controllers.Components
 
         public ActionResult GoogleMaps()
         {
-            return View(new SampleAddressModel());            
+            var model = new AddressModel
+            {
+                Address = "301 15th Street, Hood River",
+                Location = GeographyHelpers.CreatePoint(45.7120903, -121.5272902)
+            };
+
+            return View(model);
         }
 
         [HttpPost]
-        public ActionResult GoogleMaps(SampleAddressModel model)
+        public ActionResult GoogleMaps(AddressModel model)
         {
+            if (ModelState.IsValid)
+            {
+                TempData["Success"] = "Location was successfuly submitted";
+            }
             return View(model);
         }
 
--- DemoApplication/DemoApplication/Controllers/Components/ComponentsController.cs
+++ DemoApplication/DemoApplication/Controllers/Components/ComponentsController.cs
@@ -2,36 +2,12 @@
 
 namespace DemoApplication.Controllers.Components
 {
-    using Core.Common.Geography;
-    using Models.Common;
-    using Models.Components;
-
-    public class ComponentsController : Controller
     public partial class ComponentsController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult GoogleMaps()
-        {
-            var model = new AddressModel
-                {
-                    Address = "301 15th Street, Hood River",
-                    Location = GeographyHelpers.CreatePoint(45.7120903, -121.5272902)
-                };
 
-            return View(model);            
-        }
-
-        [HttpPost]
-        public ActionResult GoogleMaps(AddressModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                TempData["Success"] = "Location was successfuly submitted";
-            }
-            return View(model);
-        }
     }
 }
