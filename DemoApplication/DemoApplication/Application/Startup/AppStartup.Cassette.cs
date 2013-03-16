d6991dbcfe656ec70fc2da5dd177c781eb79be8d
Rod Johnson <rod@scaffr.com>
2013-03-16 07:38:55 -0600

    trying something else

--- DemoApplication/DemoApplication/Application/Startup/AppStartup.Cassette.cs
+++ DemoApplication/DemoApplication/Application/Startup/AppStartup.Cassette.cs
@@ -10,15 +10,13 @@ namespace DemoApplication.Application.Startup
 	{
 		public void Configure(BundleCollection configurable)
 		{
-            Bundles.RebuildCache();
-
 			configurable.AddPerSubDirectory<ScriptBundle>("Scripts", new FileSearch()
 			{
 				SearchOption = SearchOption.TopDirectoryOnly
 			});
 		
 			configurable.Add<StylesheetBundle>("content/less/site.less");
-			configurable.AddPerSubDirectory<ScriptBundle>("Scripts/custom");
+			//configurable.AddPerSubDirectory<ScriptBundle>("Scripts/custom");
 
 			configurable.AddPerSubDirectory<ScriptBundle>("Scripts/lib");
 
