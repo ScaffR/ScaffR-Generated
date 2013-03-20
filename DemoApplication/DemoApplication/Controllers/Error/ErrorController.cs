44c437dade650f3adb705ea43c916165de484b53
vcardins <vcardins@gmail.com>
2013-03-19 22:30:36 -0300

    Added Error Handling system

---
+++ DemoApplication/DemoApplication/Controllers/Error/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using DemoApplication.Filters;
+
+namespace DemoApplication.Controllers.Error
+{
+  [AllowAnonymous]
+  [ShowMainMenu(false), ShowBreadcrumb(false), ShowAlerts(false), ShowPageHeadingText(false)]
+  public class ErrorController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult NotFound()
+    {
+      return View();
+    }
+
+    public ActionResult AccessDenied()
+    {
+      return View();
+    }
+  }
+}
