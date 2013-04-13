182c14824b02ba3f4ee8cf8bc25fd4516b488ecc
Rod Johnson <rod@scaffr.com>
2013-04-12 18:55:17 -0600

    error handling, fixed gradient color for login page

---
+++ DemoApplication/DemoApplication/Controllers/Components/ComponentsController.Exceptions.cs
@@ -0,0 +1,43 @@
+﻿#region credits
+// ***********************************************************************
+// Assembly	: DemoApplication
+// Author	: Rod Johnson
+// Created	: 03-20-2013
+// 
+// Last Modified By : Rod Johnson
+// Last Modified On : 03-28-2013
+// ***********************************************************************
+#endregion
+namespace DemoApplication.Controllers.Components
+{
+    #region
+
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using Filters;
+    using Models.Components;
+
+    #endregion
+
+    public partial class ComponentsController
+    {
+
+        //private readonly Common.Logging.ILog _logger = Common.Logging.LogManager.GetCurrentClassLogger();
+        //
+        // GET: /Logging/
+
+        [Log, AllowAnonymous]
+        public ActionResult Exceptions()
+        {            
+            return View();
+        }
+
+        [HttpPost,AllowAnonymous]
+        public ActionResult Throw500()
+        {            
+            throw new NotImplementedException("This should give a 500 error");
+        }
+        
+    }
+}
